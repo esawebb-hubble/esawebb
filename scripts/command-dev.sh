742afa84426041e20257723d45151d7a98548ab2
Alejandro Vivas <alejo.vivas.av@gmail.com>
2025-01-07 12:57:21 -0500

    Make migrations before aplied it

--- scripts/command-dev.sh
+++ scripts/command-dev.sh
@@ -3,6 +3,7 @@
 # Copy test media that will provide extra coverage for templates
 /bin/cp -rf test-utils/media/* docs/static
 
+python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata initial dev
 python manage.py runserver 0.0.0.0:8000
